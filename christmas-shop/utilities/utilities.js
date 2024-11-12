ee21145a6cd20d6850569b414cc997422233d6d6
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-11-12 22:18:44 +0300

    feat: add up btn

--- christmas-shop/utilities/utilities.js
+++ christmas-shop/utilities/utilities.js
@@ -5,3 +5,19 @@ export function createElement(tagName, className = "", text = "") {
 
     return element;
 }
+
+const upBtn = document.querySelector(".up-btn");
+upBtn.style.display = 'none';
+
+window.addEventListener("scroll", function () {
+    const screenHeight = this.document.documentElement.clientHeight;
+
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+    console.log(scrollY);
+    scrollY > 0 ? upBtn.style.display = 'flex' : upBtn.style.display = 'none';
+});
+
+/* window.addEventListener('resize', function() {
+    const новаяШирина = window.innerWidth;
+    const новаяВысота = window.innerHeight;
+  }); */
