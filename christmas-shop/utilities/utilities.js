ae847cec321979b9128bd3e9a0fcd58379234d8f
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-11-22 20:52:23 +0300

    fix: update logo

--- christmas-shop/utilities/utilities.js
+++ christmas-shop/utilities/utilities.js
@@ -22,4 +22,4 @@ if (upBtn) upBtn.style.display = 'none';
   }); */
 
 
-console.log('Для корректной проверки на экране < 1440px в DevTools выберите Мобильный режим - 1440 - Mobile(no touch) (В ТЗ:Please note that when checking the work in a window with a width of 1440 pixels, the layout may compress by approximately 17 pixels. This happens because part of the layout space is consumed by the vertical scroll (17 pixels - the standard scroll size for Google Chrome)) P.S В режиме Mobile(no touch) hover будет срабатывать. Для проверки отсутствия hover на моб устройствах нужно выбирать редми Moble.')
+console.log('Для корректной проверки на экране < 1440px в DevTools выберите Мобильный режим - 1440 - Mobile(no touch) (В ТЗ:Please note that when checking the work in a window with a width of 1440 pixels, the layout may compress by approximately 17 pixels. This happens because part of the layout space is consumed by the vertical scroll (17 pixels - the standard scroll size for Google Chrome)) P.S В режиме Mobile(no touch) hover будет срабатывать. Для проверки отсутствия hover на моб устройствах нужно выбирать режим Mobile.')
