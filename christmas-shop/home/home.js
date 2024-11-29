c0816bb2962f8810b97e82528832d37d0643c0b4
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-11-29 18:47:17 +0300

    feat: render gifts cards on Home page

--- christmas-shop/home/home.js
+++ christmas-shop/home/home.js
@@ -1,8 +1,14 @@
+import gifts from '../data/gifts.json' with {type: 'json'};
 import { handleBurger } from "../utilities/header.js";
+import { renderCards } from '../utilities/utilities.js';
+
+const cardsAmount = 4;
+const giftCardsWrapp = document.querySelector('.gift-cards-wrapper');
 
 
 init();
 
 function init() {
     handleBurger();
+    renderCards(giftCardsWrapp, gifts, cardsAmount);
 }
