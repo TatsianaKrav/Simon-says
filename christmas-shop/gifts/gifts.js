8b141acbc48bf9e66c7d920d56c69af34846572d
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-11-29 19:06:47 +0300

    feat: add up btn

--- christmas-shop/gifts/gifts.js
+++ christmas-shop/gifts/gifts.js
@@ -1,8 +1,35 @@
 import { handleBurger } from "../utilities/header.js";
 
+const upBtn = document.querySelector(".up-btn");
+let curretWidthScreen = document.documentElement.clientWidth;
 
 init();
 
 function init() {
     handleBurger();
+
+    if (curretWidthScreen <= 768) {
+        handleUpBtn();
+    } else {
+        upBtn.style.opacity = '0';
+    }
+}
+
+
+window.addEventListener('resize', function () {
+    curretWidthScreen = document.documentElement.clientWidth;
+
+    if (curretWidthScreen <= 768) {
+        handleUpBtn();
+    } else {
+        upBtn.style.opacity = '0';
+    }
+});
+
+function handleUpBtn() {
+    window.addEventListener("scroll", function () {
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+
+        scrollY > 300 && curretWidthScreen <= 768 ? upBtn.style.opacity = '1' : upBtn.style.opacity = '0';
+    });
 }
--- christmas-shop/utilities/utilities.js
+++ christmas-shop/utilities/utilities.js
@@ -6,8 +6,7 @@ export function createElement(tagName, className = "", text = "") {
     return element;
 }
 
-const upBtn = document.querySelector(".up-btn");
-if (upBtn) upBtn.style.display = 'none';
+
 
 
 export function navigate(links) {
@@ -127,18 +126,6 @@ function getRandom(amount, length) {
 
     return randomNums;
 }
-
-/* window.addEventListener("scroll", function () {
-    const scrollY = window.scrollY || document.documentElement.scrollTop;
-    if (!upBtn) return;
-
-    scrollY > 0 ? upBtn.style.display = 'flex' : upBtn.style.display = 'none';
-}); */
-
-/* window.addEventListener('resize', function() {
-    const новаяШирина = window.innerWidth;
-    const новаяВысота = window.innerHeight;
-  }); */
 
 
 console.log('Для корректной проверки на экране < 1440px в DevTools выберите Мобильный режим - 1440 - Mobile(no touch) (В ТЗ:Please note that when checking the work in a window with a width of 1440 pixels, the layout may compress by approximately 17 pixels. This happens because part of the layout space is consumed by the vertical scroll (17 pixels - the standard scroll size for Google Chrome)) P.S В режиме Mobile(no touch) hover будет срабатывать. Для проверки отсутствия hover на моб устройствах нужно выбирать режим Mobile.')
