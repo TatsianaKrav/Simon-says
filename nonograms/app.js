6725c97cddd10eedfe6998f5f988a475915fe3a6
Татьяна Кравченко <tats.krauchanka@gmail.com>
2025-02-04 00:14:08 +0300

    feat: add console message

--- nonograms/app.js
+++ nonograms/app.js
@@ -3,3 +3,5 @@ import { Game } from "./src/game.js";
 const game = new Game();
 localStorage.removeItem('theme');
 game.init('Easy');
+
+console.log('Для удобства проверки можно "зачеркивать"подсказки нажатием левой кнопки мыши');
